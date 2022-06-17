964cd0e4657989b180106bfda61de2ae1f0329ba
Brian Dietz <brian.dietz@gmail.com>
2022-06-17 10:33:41 -0400

    Update where_is_your_iso.sh

--- scrc_scripts/where_is_your_iso.sh
+++ scrc_scripts/where_is_your_iso.sh
@@ -5,5 +5,5 @@ echo Where is your iso?
 read path_to_iso
 
 # will need to update volume path for other users; alt, could make that a variable as well
-docker run -v /Users/bjdietz/archives/working/born_digital/to-process:/Users/bjdietz/archives/working/born_digital/to-process:ro --rm focal:bdap 
+docker run -v /Users/"$(whoami)"/archives/working/born_digital/to-process:/Users/"$(whoami)"/archives/working/born_digital/to-process:ro --rm focal:bdap 
 /bin/bash -c "disktype $path_to_iso"
